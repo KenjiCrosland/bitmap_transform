116a7f10ff63241763e6a7f1f80954412886c232
Kenji Crosland <kenji.crosland@gmail.com>
2015-10-29 19:24:17 -0700

    Filling out gulpfile with jshint and mocha tasks

--- bitmap.js
+++ bitmap.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
 var bitmap = fs.readFileSync('palette-bitmap.bmp');
-console.log(bitmap.toString('utf-8', 0, 2));
-console.log('size: ' + bitmap.readUInt32LE(2) + 'bytes');
-console.log('pixel data start: ' + bitmap.readUInt32LE(10));
-console.log('bit depth: ' + bitmap.readUInt16LE(28));
-console.log('number of colors: ' + bitmap.readUInt32LE(46));
+
+var convertBitmap = module.exports = exports = function convertBitmap(bitmap){
+  console.log(bitmap.toString('utf-8', 0, 2));
+  console.log('size: ' + bitmap.readUInt32LE(2) + 'bytes');
+  console.log('pixel data start: ' + bitmap.readUInt32LE(10));
+  console.log('bit depth: ' + bitmap.readUInt16LE(28));
+  console.log('number of colors: ' + bitmap.readUInt32LE(46));
+};
