d9400fc843170237e3d9b1fff2f857b801e1474c
Kenji Crosland <kenji.crosland@gmail.com>
2015-10-29 18:38:15 -0700

    Add bitmap files. Change file to read. Add 5 empty tests

---
+++ test/test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var expect = require('chai').expect;
+var bitmap = require(__dirname + '/../bitmap.js');
+
+describe('the bitmap processing function', function(){
+  it('should open a bitmap using fs and read it into a buffer', function() {
+
+  });
+
+  it('should convert the buffer into a JavaScript Object', function() {
+
+  });
+
+  it('should invert the colors in the bitmap', function() {
+
+  });
+
+  it('should turn the transformed object back into a buffer', function(){
+
+  });
+
+  it ('should write that buffer to a new file', function(){
+
+  });
+});
